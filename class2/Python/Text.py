468342f5d9ce22ef03394c82124fff32aeacca85
shahzad khan <shahzadkhanz@gmail.com>
2022-05-31 18:28:10 +0500

    class2

--- class2/Python/Text.py
+++ class2/Python/Text.py
@@ -1,5 +1,3 @@
 print("Shahzad Khan")
-Print("Saylani AI Batch 3")
-Print("Gulshan Campus")
-print("Monday & Friday")
-Print("Timmings: 9:00 - 12:00")
+print("Saylani Batch 3")
+print("Gulshan Campus")
